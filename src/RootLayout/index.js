8fee15546bc4b6f408ff120cd50c467ad03807eb
shahanshah <mohommed.shanshah@upgrad.com>
2023-09-02 08:34:42 +0530

    remove white space

--- src/RootLayout/index.js
+++ src/RootLayout/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "../Components/Header";
-import Styles from "./index.module.scss";
 import FooterSection from "../Components/FooterSection";
 import MapSection from "../Components/MapSection";
 import { isMobile } from "react-device-detect";
